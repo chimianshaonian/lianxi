a001608ac7dac41b1c0e28a868125bd0a9063930
田法旺 <14738866+tianfawang@user.noreply.gitee.com>
2024-08-03 15:31:07 +0800

    1、私有权限更新 2、新增多态文件

--- 接口第二天8-1/1、面向对象继承_继承/6、私有权限.py
+++ 接口第二天8-1/1、面向对象继承_继承/6、私有权限.py
@@ -22,6 +22,44 @@ class Fulei:
     def __get_money(self):
         print(self.__money)
 
+    """
+    一般定义获取和修改私有属性值的命名习惯
+    获取：get_XX  用来获取私有属性
+    修改：set_XX  用来修改私有属性值
+    """
+
+    # 私有的属性和方法，只能在内部进行调用
+    def set_money(self,n):
+        if n > 0:
+            self.__money += n
+        else:
+            print('攒钱呢')
+
+    def print_money(self):
+        self.__get_money()
+
+# 子类
+class Erzi(Fulei):
+    pass
+
+# 调用
+if __name__ == '__main__':
+    xiaoming = Erzi()
+    print(xiaoming.shouyi)
+    xiaoming.zhizuo()  # 五香制作
+    # print(xiaoming.__money)     # 报错，无法直接调用父类的私有属性
+    # xiaoming.__get_money()      # 报错，无法直接调用父类的私有方法
+    xiaoming.print_money()     # 200
+    xiaoming.set_money(100)
+    xiaoming.print_money()     # 300，此时父类的私有属性money = 300
+    xiaoming.set_money(-100)   # 攒钱呢
+    xiaoming.print_money()
+
+# 查看类的属性和方法
+print(Fulei.__dict__)   # 类的属性和方法
+print(xiaoming.__dict__)  # 只能看到类的方法
+print(xiaoming._Fulei__money)
+xiaoming._Fulei__get_money()
 
 
 
